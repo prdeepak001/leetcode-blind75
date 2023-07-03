77bba0162f706c6cb7ba5af74e96e827e7d4fc43
PRDeepak <deepakpraman@icloud.com>
2023-07-03 23:58:24 +0400

    Arrays

---
+++ data-structures/src/com/bigdreams/leetcode/trees/TreeNode.java
@@ -0,0 +1,11 @@
+package com.bigdreams.leetcode.trees;
+
+public class TreeNode<T> {
+    T data;
+    TreeNode<T>leftNode;
+    TreeNode<T>rightNode;
+
+    public TreeNode(T data) {
+        this.data = data;
+    }
+}
